3b5738363a98612e737cf4f5e1851ae4f2e5b981
Maximilian Held <info@maxheld.de>
2017-10-03 23:10:01 +0200

    streamline all item handles closes #36

--- rendercards.R
+++ rendercards.R
@@ -3,7 +3,7 @@
 concourse <- readxl::read_xlsx(path = "concourse.xlsx", sheet = 1)
 for (i in concourse$handle) {
   item_text <- as.character(concourse[concourse$handle == i, "german"])
-  item_handle <- as.character(concourse[concourse$handle == i, "kurz"])
+  item_handle <- i
   pensieve:::make_cards(item_text = item_text,
                         item_handle = item_handle,
                         output_dir = file.path(getwd(), "zaw17app/www/cards"),
