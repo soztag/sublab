538fb3e6e67a9bb12422ddf158d18a838732bea0
Maximilian Held <info@maxheld.de>
2017-10-04 13:48:19 +0200

    import all zaw data

--- setup.R
+++ setup.R
@@ -13,6 +13,7 @@ library(stringr)
 library(plyr)
 library(xtable)
 library(pander)
+library(abind)
 if (interactive()) {
   install.packages("../pensieve", repos = NULL, type = "source", INSTALL_opts = c('--no-lock'))
   install.packages("../qmethod", repos = NULL, type = "source", INSTALL_opts = c('--no-lock'))
