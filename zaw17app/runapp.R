50aaa5b54a9ec35ad00dee0d61d127a76e68fc20
Maximilian Held <info@maxheld.de>
2017-10-04 04:15:51 +0200

    fix last issues with fs filesystem

--- zaw17app/runapp.R
+++ zaw17app/runapp.R
@@ -1,4 +1,4 @@
-shiny::runApp(appDir = 'C:/Users/user/Desktop/',
+shiny::runApp(appDir = 'C:/Users/user/Desktop/zaw17app-desirable',
               port = 4063,
               host = "127.0.0.1"
               )
