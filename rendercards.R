c62c4800a93fba69a3911eb460c204ff533a9c46
Maximilian Held <info@maxheld.de>
2018-04-19 08:52:05 +0200

    make small corrections

--- rendercards.R
+++ rendercards.R
@@ -27,5 +27,5 @@ for (i in concourse$handle) {
 devtools::install_github(repo = "maxheld83/qmethod")
 library(qmethod)
 c4print <- matrix(data = concourse$english, dimnames = list(item = concourse$handle, language = "english"))
-qmethod::make.cards(q.set = c4print, study.language = "english", output.pdf = TRUE, duplex.double = FALSE, wording.font.size = "\\LARGE")
+qmethod::make.cards(q.set = c4print, study.language = "english", output.pdf = TRUE, duplex.double = FALSE, wording.font.size = "\\Large", show.handles = TRUE)
 help("make.cards")
